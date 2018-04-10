75d8f6cbf7004bbb93adaf666ffcb618bd43bb77
ruixian102 <ruixian@etechnologycentre.com>
2018-04-10 10:03:17 +0800

    small changed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged out!
+                    You are logged innnnnn!
                 </div>
             </div>
         </div>
